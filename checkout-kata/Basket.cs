4a0875fda6e34ff520d0c049a231decacc8c8f7c
mkent92 <62265337+mkent92@users.noreply.github.com>
2020-03-19 16:43:41 +0000

    Add files via upload

---
+++ checkout-kata/Basket.cs
@@ -0,0 +1,25 @@
+﻿namespace checkout_kata
+{
+    public class Basket
+    {
+        private PricingRules _pricingRules;
+        private int _total;
+
+        public Basket(PricingRules pricingRules)
+        {
+            _pricingRules = pricingRules;
+        }
+
+        public void Scan(Item item)
+        {
+            _total += item.Price;
+        }
+
+
+        public int GetTotal()
+        {
+            return _total;
+        }
+
+    }
+}
---
+++ checkout-kata/Item.cs
@@ -0,0 +1,29 @@
+﻿namespace checkout_kata
+{
+    public class Item
+    {
+        #region Properties
+        public string Sku
+        {
+            get;
+            set;
+        }
+
+        public string Name
+        {
+            get;
+            set;
+        }
+
+        public int Price
+        {
+            get;
+            set;
+        }
+        #endregion
+
+        public Item()
+        {
+        }
+    }
+}
