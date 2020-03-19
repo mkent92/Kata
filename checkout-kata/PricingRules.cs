081c1748ae600681da8a7adba682c9c2a6ef92be
mkent92 <62265337+mkent92@users.noreply.github.com>
2020-03-19 16:54:19 +0000

    Add files via upload

---
+++ checkout-kata/PricingRules.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace checkout_kata
+{
+    public class PricingRules
+    {
+        private Dictionary<string, int> _rules = new Dictionary<string, int>();
+
+        public PricingRules(Dictionary<string, int> rules)
+        {
+            _rules = rules;
+        }
+
+        public int? LookupPrice(string sku, int units)
+        {
+            string key = String.Format("{0}:{1}", sku, units);
+            return _rules.ContainsKey(key) ? (int?)_rules[key] : null;
+        }
+    }
+}
